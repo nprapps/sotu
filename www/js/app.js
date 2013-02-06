121014e2e95f522f315b31b45447587c9792369f
Katie Zhu <kzhu91@gmail.com>
2013-02-06 15:52:40 -0600

    pull schedule and credits into modal on mobile. [closes #57]

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,7 @@
 $(function() {
+    var schedule = JST.schedule();
+    $('#schedule .modal-body').html(schedule);
+
     $('#live-chat').livechat({
         chat_id: APP_CONFIG['CHAT']['ID'],
         chat_token: APP_CONFIG['CHAT']['TOKEN'],
@@ -6,4 +9,6 @@ $(function() {
         alert_interval: 500,
         read_only: false
     });
+
+    $('.chat-schedule-wrap').append(schedule);
 });
