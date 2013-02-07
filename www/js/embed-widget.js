78608850721a7915ab7b48322565716212cd0204
Christopher Groskopf <staringmonkey@gmail.com>
2013-02-07 14:26:59 -0600

    Add missing widget ad tags. Closes #78.

--- www/js/embed-widget.js
+++ www/js/embed-widget.js
@@ -9,7 +9,7 @@ try {
         var pageTracker = _gat._getTracker("UA-5828686-3");
         pageTracker._setDomainName("none");
         pageTracker._setAllowLinker(true);
-        pageTracker._setCustomVar(1,"Module","TODO",3);
+        pageTracker._setCustomVar(1,"Module","stateoftheunion2013",3);
         pageTracker._trackPageview();
 } catch(err) {}
 
@@ -28,7 +28,7 @@ try {
 }
 
 document.write(
-'<script type="text/javascript">window.vsitag = {"imp":"TODO"};</script>',
+'<script type="text/javascript">window.vsitag = {"imp":"stateoftheunion2013"};</script>',
 '<script type="text/javascript" language="javascript" src="http://www.npr.org/include/javascript/zigi.js"></script>',
 '<iframe src="widget.html?station=' + window.station + '" width="' + nprapps_widget_width + '" height="' + nprapps_widget_height + '" scrolling="auto" marginheight="0" marginwidth="0" frameborder="0" style="border: 1px solid #CCC;" ></iframe>',
 '');
